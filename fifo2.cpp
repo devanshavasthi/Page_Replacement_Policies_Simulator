28fb6c67cbeb359a34790930fb5ee5917ee6f97e
K. Yasaswi Sri Chandra Gandhi <yasaswisrichandragandhi@gmail.com>
2021-11-17 20:57:04 +0530

    comments to explain code

--- fifo2.cpp
+++ fifo2.cpp
@@ -30,14 +30,21 @@ void printSet(unordered_set<int> &frameSet, int page, bool col)
 
 void FIFO2Replacement(int frames, vector<int> &pageSeq)
 {
+    // set toggle bits for each frame to count 2nd chance
     int chanceBit[2][frames];
     int frameptr = 0, pageptr = 0, misses = 0, flag = 0, load = 0;
+
+    // initialize all chance bits to a negative number (to allow for 0's
+    // to be used in input)
     for (int i = 0; i < 2; i++)
         for (int j = 0; j < frames; j++)
             chanceBit[i][j] = -7;
+
     int pageSeqsize = pageSeq.size();
     while (pageptr < pageSeqsize)
     {
+        // Check if the page is already in the frame set and set the toggle
+        // bit to 1 if it is.
         for (int i = 0; i < frames; i++)
         {
             if (chanceBit[0][i] == pageSeq[pageptr])
@@ -48,8 +55,11 @@ void FIFO2Replacement(int frames, vector<int> &pageSeq)
         }
         if (flag == 0)
         {
+            // We only move to the next frame if there is no load in the
+            // current iteration.
             do
             {
+                // Loads the page into the frames and sets the toggle bit to 0.
                 if (chanceBit[1][frameptr] == 0)
                 {
                     chanceBit[0][frameptr] = pageSeq[pageptr];
@@ -57,17 +67,24 @@ void FIFO2Replacement(int frames, vector<int> &pageSeq)
                     misses++;
                     load = 1;
                 }
+                // Invoke a chance and flip the bit back to 0
                 else
                     chanceBit[1][frameptr] = 0;
                 frameptr++;
+
+                // Cycle back to the beginning
                 if (frameptr == frames)
                     frameptr = 0;
             } while (load != 1);
         }
+
+        // Adding frames to a set to print later
         unordered_set<int> frameSet;
         for (int i = 0; i < frames; i++)
             if (chanceBit[0][i] != -7)
                 frameSet.insert(chanceBit[0][i]);
+
+        // Print the set
         cout << "\033[0;37m" << pageSeq[pageptr] << "--->";
         printSet(frameSet, pageSeq[pageptr], flag);
         printf("\n\n");
@@ -75,6 +92,8 @@ void FIFO2Replacement(int frames, vector<int> &pageSeq)
         load = 0;
         pageptr++;
     }
+
+    // Stats
     printf("\n");
     int hits = pageSeqsize - misses;
     printf("Hits: %d\n", hits);
@@ -89,9 +108,9 @@ int main()
 
     printf("Enter the number of frames: ");
     scanf("%d", &frames);
-    if (frames < 1)
+    if (frames < 1 || frames > 99)
     {
-        printf("The number of frames should be greater than 1\n");
+        printf("The number of frames should be between 1 and 99.\n");
         return 0;
     }
 
